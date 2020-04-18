9ef30beff71f142b6a521ed1090eebbf2f97ae6b
moussadiop2005 <62149726+moussadiop2005@users.noreply.github.com>
2020-04-18 09:24:02 +0000

    Update Interface_admin.php

--- Interface_admin.php
+++ Interface_admin.php
@@ -16,14 +16,14 @@ if ($_SESSION['connecte']!=1) {
 </head>
 <body>
     <div id="body">
-        <header>
-            <img src="asset/IMG/logo-QuizzSA.png" width="58" class="logo_header" >
-            <div id='header'>Le plaisir de jouer</div>
-        </header>
+        <div class="header">
+            <div class="logo_header" ><img src="asset/IMG/logo-QuizzSA.png" width="58"></div>
+            <div class="text_header">Le plaisir de jouer</div>
+        </div>
         <div class="div">
             <div class="div1">
-                <div>CRÉER ET PARAMÉRTER VOS QUIZZ </div>
-                <form method="post"><button type="submit" name="logout">Déconnexion</button></form>
+                <div class="div11">CRÉER ET PARAMÉRTER VOS QUIZZ </div>
+                <form method="post" class="logout"><button type="submit" name="logout">Déconnexion</button></form>
             </div>
             <?php
             if (isset($_POST['logout'])) {
@@ -32,8 +32,8 @@ if ($_SESSION['connecte']!=1) {
             }
             ?>
             <div class="div2">
-                <div class="profil">
-                    <div class="img">
+                <div class="nav">
+                    <div class="profil">
                         <img src="<?=$_SESSION['admin_actuel']['photo']?>" width="110">
                         <div class="identifiant">
                             <div><?=strtoupper($_SESSION['admin_actuel']['prenom'])?></div>
@@ -41,45 +41,77 @@ if ($_SESSION['connecte']!=1) {
                         </div>
                     </div>
                     <nav class="menu">
-                    <form method="post"></form>
-                        <div >
-                            <label for="liste_question" class="titre_menu"><span>Liste Questions</span>
-                            <input type="radio" name="menu" id="liste_question" value="1">
-                            <img src="asset/IMG/Icônes/ic-liste.png" class="inactive">
-                            <img src="asset/IMG/Icônes/ic-liste-active.png" class="active">
-                            </label>
-                        </div><br>
-                        <div>
-                            <label for="Creer_Admin" class="titre_menu"><span>Créer Admin</span>
-                            <input type="radio" name="menu" id="Creer_Admin" value="2">
-                            <img src="asset/IMG/Icônes/ic-ajout.png" class="inactive">
-                            <img src="asset/IMG/Icônes/ic-ajout-active.png" class="active">
-                            </label>
-                        </div><br>
-                        <div>
-                            <label for="liste_joueur" class="titre_menu"><span>Liste joueurs</span>
-                            <input type="radio" name="menu" id="liste_joueur" value="3">
-                            <img src="asset/IMG/Icônes/ic-liste.png" class="inactive">
-                            <img src="asset/IMG/Icônes/ic-liste-active.png" class="active">
-                            </label>
-                        </div><br>
-                        <div>
-                            <label for="creer_question" class="titre_menu"><span>Créer Questions</span>
-                            <input type="radio" name="menu" id="creer_question" value="2">
-                            <img src="asset/IMG/Icônes/ic-ajout.png" class="inactive">
-                            <img src="asset/IMG/Icônes/ic-ajout-active.png" class="active">
-                            </label>
-                        </div>
+                        <ul>
+                            <li class="submenu active">
+                                <a href="#liste_question" class="titre_menu">Liste Questions<div class="ic-liste"></div></a>
+                            </li>
+                            <li>
+                                <a href="#creer_admin" class="titre_menu">Créer Admin<div class="ic-ajout"></div></a>
+                            </li>
+                            <li>
+                                <a href="#liste_joueur" class="titre_menu">Liste joueurs<div class="ic-liste"></div></a>
+                            </li>
+                            <li>
+                                <a href="#creer_question" class="titre_menu">Créer Questions<div class="ic-ajout"></div></a>
+                            </li>
+                        <ul>
                     </nav>
                 </div>
                 <div class="content">
-                qsdf
+                    <div class="content-onglet active" id="liste_question">
+                        <div class="form">
+                            <div class="head-form">
+                                <div class="title-form">S'INCRIRE</div>
+                                <div class="text-head">Pour proposer des quizz</div>
+                            </div>
+                            <hr>
+                            <div class="body-form">
+                                <label for="prenom" class="label-form">Prénom</label><br>
+                                <input type="text" class="input-form" id="prenom" name="prenom"><br>
+                                <label for="nom" class="label-form">Nom</label><br>
+                                <input type="text" class="input-form" id="nom" name="nom"><br>
+                                <label for="login" class="label-form">Login</label><br>
+                                <input type="text" class="input-form" id="login" name="login"><br>
+                                <label for="pwd" class="label-form">Password</label><br>
+                                <input type="password" class="input-form password" id="pwd" name="pwd"><br>
+                                <label for="confirm" class="label-form">Confirmer Password</label><br>
+                                <input type="password" class="input-form password" id="confirm" name="confirm"><br>
+                                <label for="avatar" class="label-avatar">Avatar</label>
+                                <input type="file" class="input-avatar" value="Choisir un fichier"><br>
+                                <button type="submit" name="submit-compte" class="submit-compte">Créer compte</button>
+                            </div>
+                        </div>
+                        <div class="photo">
+                            <div class="avatar"></div>
+                            <div class="text-avatar">Avatar</div>
+
+                        </div>
+                    </div>
+                    <div class="content-onglet" id="creer_admin">
+                        <div class="nbr">
+                            <label for="nbr" class="nbr-label">Nombre de questions/Jeu</label>
+                            <input type="text" id="nbr" name="nbr_question" class="nbr-input">
+                            <button type="submit" name="submit_nbr" class="nbr-ok">OK</button>
+                        </div><br>
+                        <div>
+                            <div class="questions">
+
+                            </div><br>
+                            <button type="submit" name="precedent" class="precedent">Précedent</button>
+                            <button type="submit" name="suivant" class="suivant">Suivant</button>
+                        </div>
+                    </div>
+                    <div class="content-onglet" id="liste_joueur">
+
+                    </div>
+                    <div class="content-onglet" id="creer_question">
+                        
+                    </div>
                 </div>
-            
             </div>
         </div>
     </div>
     
-    
+    <script type="text/javascript" src="asset/JS/Interface_admin.js"></script>
 </body>
 </html>
